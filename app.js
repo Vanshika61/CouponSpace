3616cedf5ca161d7b31ec3e478265898de4470e3
Vanshika <agrawal10614@gmail.com>
2023-06-29 13:35:12 +0530

    rendering data to the website

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
+const axios = require('axios');
+const pug = require('pug');
 const port = 8000;
 
 const mongoose = require('mongoose');
@@ -12,7 +14,6 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/couponSpace');
 }
 
-
 // Define mongoose schema
 const contactSchema = new mongoose.Schema({
   name: String,
@@ -21,28 +22,27 @@ const contactSchema = new mongoose.Schema({
   couponName: String,
   details: String,
   expiry: Date
-  // couponImg: String, // Update to store the file path instead of the File itself
 });
 
-
 const Contact = mongoose.model('Contact', contactSchema);
-
 
 // EXPRESS SPECIFIC CONFIGURATION
 app.use('/static', express.static('static')); // For serving static files
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 // PUG SPECIFIC CONFIGURATION
 app.set('view engine', 'pug'); // Set the template engine as pug
 app.set('views', path.join(__dirname, 'views')); // Set the views directory
 
-
-
 // ENDPOINTS CONFIGURATION
-app.get('/', (req, res) => {
-  const params = {};
-  res.status(200).render('index', params);
+app.get('/', async (req, res) => {
+  try {
+    const data = await Contact.find({}).exec();
+    res.render('index', { data });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 app.get('/contact', (req, res) => {
@@ -64,7 +64,7 @@ app.post('/contact', (req, res) => {
     });
 });
 
-app.get('/data', (req, res) => {
+app.get('/api/data', (req, res) => {
   Contact.find({})
     .exec()
     .then((data) => {
@@ -76,18 +76,15 @@ app.get('/data', (req, res) => {
     });
 });
 
-app.get('/data', (req, res) => {
-  Contact.find({})
-    .exec()
-    .then((data) => {
-      res.render('index', { data }); // Pass the data to the 'index' template
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
-    });
+app.get('/', async (req, res) => {
+  try {
+    const data = await Contact.find({}).exec();
+    res.render('index', { data });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
 });
-
 
 // START THE SERVER
 app.listen(port, () => {
